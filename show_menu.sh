483c4ef186f984d94d1ea296bb9eababbea5e16b
James Prankard <prankard@gmail.com>
2020-12-11 20:12:36 +0000

    updated show to unzip

--- show_menu.sh
+++ show_menu.sh
@@ -20,19 +20,20 @@ function show_advanced_menu()
                 local TMP_LIST=$TEMP_FOLDER/list.txt
                 find "$rom_folder" -name '*.smc' -o -name '*.sfc' > $TMP_LIST
                 local chosen_file_path=$(eval chooseOneOption "\"$TMP_LIST\"" "\" A Choose ROM \"" "\"\nPlease Select an source UNHEADERED rom file to use to patch homebrew\n\nThis will be the base file that all hacks will be patched from\n\"")
+                local destination_file=$(eval getSourceGamePath "$game_choice")
                 if [ ! -z "$chosen_file_path" ]; then
                     local extension="${chosen_file_path##*.}"
                     local extension=${extension,,} #to lowercase
                     if [ "$extension" = "zip" ]; then
                         #dialog --title "  Oh noes  " --colors --msgbox "Oh now, we can't do zip just yet:\n$chosen_file_path\n" 19 80
                         #todo, find a way to do multiple file types
-                        7z l "$chosen_file_path" | grep -i '.sfc' | cut -c 54- > $TMP_LIST 
+                        7z l "$chosen_file_path" | grep -i '.smc' | cut -c 54- > $TMP_LIST 
                         local zip_choice=$(eval chooseOneOption "\"$TMP_LIST\"" "\" Choose a source file \"" "\"Please select and source file to patch\"")
                         if [ ! -z "$zip_choice" ]; then
                             #unzip -p "$TMP_ARCHIVE" "$ips_files" > "$TMP_IPS" # old unzip
                             cd $TEMP_FOLDER
-                            7z e "$TMP_ARCHIVE" "$zip_choice"
-                            local choice_filename=$(basename -- "$choice") #get filename
+                            7z e "$chosen_file_path" "$zip_choice"
+                            local choice_filename=$(basename -- "$zip_choice") #get filename
                             mv "${TEMP_FOLDER}/${choice_filename}" "${destination_file}"
                             cd "$ROOT_DIR"
                         else
@@ -40,7 +41,6 @@ function show_advanced_menu()
                         fi
 
                     else
-                        local destination_file=$(eval getSourceGamePath "$game_choice")
                         local valid_md5_hash=$(getMd5FromGame "${game_choice}")
                         local hash=($(md5sum "$chosen_file_path"))
                         if [ "$hash" = "$valid_md5_hash" ]; then
